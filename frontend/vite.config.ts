85add4fed11a6d731d562202cf70282b4b107a44
EitanMir <eitanmiron234@gmail.com>
2025-12-12 01:14:48 -0500

    Switch MUI to styled-components engine to fix Vite build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    },
+  },
   optimizeDeps: {
     include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
   },
