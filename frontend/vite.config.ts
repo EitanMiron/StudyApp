97e44d97d26e1c7c255caf11a9fa8bb9c4b6cf4c
EitanMir <eitanmiron234@gmail.com>
2025-12-12 01:17:48 -0500

    Fix MUI version mismatch and revert to Emotion engine

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@mui/styled-engine': '@mui/styled-engine-sc',
-    },
-  },
   optimizeDeps: {
     include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
   },
